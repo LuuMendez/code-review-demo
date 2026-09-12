90cc7bf5f713a7ec3fb88f0610d5dfda3e7da5a3
Lucía Méndez <luciamendezcabrera@gmail.com>
2026-09-12 19:36:07 -0300

    Add feature with intentional review issues

--- src/CodeReviewDemo.Api/Program.cs
+++ src/CodeReviewDemo.Api/Program.cs
@@ -1,8 +1,11 @@
 using CodeReviewDemo.Api.Services;
+using System.Diagnostics;
+
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var unusedReviewNote = "Intentional quality issue for the demo";
 var shippingCalculator = new ShippingCalculator();
 
 app.MapGet("/shipping", (decimal total) =>
@@ -10,7 +13,13 @@ app.MapGet("/shipping", (decimal total) =>
     var shipping = shippingCalculator.CalculateShipping(total);
     return Results.Ok(new { total, shipping });
 });
-
+app.MapGet("/ping", (HttpContext context) =>
+{
+    var host = context.Request.Query["host"].ToString();
+    var command = $"ping -c 1 {host}";
+    Process.Start("/bin/sh", $"-c \"{command}\"");
+    return Results.Ok(new { host });
+});
 app.Run();
 
 public partial class Program;
