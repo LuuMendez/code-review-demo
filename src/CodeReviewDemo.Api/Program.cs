0be90cfb71cf8df71ff4f2d6d774fb797d562a42
martinarebellato <marti.rebellato@gmail.com>
2026-09-12 20:26:33 -0300

    Introduce three intentional code review issues

--- src/CodeReviewDemo.Api/Program.cs
+++ src/CodeReviewDemo.Api/Program.cs
@@ -1,14 +1,24 @@
 using CodeReviewDemo.Api.Services;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var unusedReviewNote = "Intentional quality issue for the demo";
 var shippingCalculator = new ShippingCalculator();
 
 app.MapGet("/shipping", (decimal total) =>
 {
     var shipping = shippingCalculator.CalculateShipping(total);
     return Results.Ok(new { total, shipping });
+});
+
+app.MapGet("/ping", (HttpContext context) =>
+{
+    var host = context.Request.Query["host"].ToString();
+    var command = $"ping -c 1 {host}";
+    Process.Start("/bin/sh", $"-c \"{command}\"");
+    return Results.Ok(new { host });
 });
 
 app.Run();
